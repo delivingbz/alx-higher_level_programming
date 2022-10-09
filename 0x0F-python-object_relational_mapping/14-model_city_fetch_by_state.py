b67a37afd106592fe5877f2b7471a9e3c0d28c3f
kingsley <kingscode2@gmail.com>
2022-10-09 15:07:32 -0700

    fix

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-"""
-Prints all City objects from the database hbtn_0e_14_usa
-"""
-import sys
-from model_state import Base, State
-from model_city import City
+""" lists all cities objects from the database hbtn_0e_6_usa """
+
+from sys import argv
 from sqlalchemy import create_engine
+from model_city import City
+from model_state import State
 from sqlalchemy.orm import sessionmaker
 
-
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
-    st_cty = session.query(State, City).filter(State.id == City.state_id).all()
+    session_maker = sessionmaker(bind=engine)
+    session = session_maker()
 
-    for state, city in st_cty:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for city, state in session.query(City, State)\
+                              .filter(City.state_id == State.id)\
+                              .order_by(City.id):
+        print(f"{state.name}: ({city.id}) {city.name}")
